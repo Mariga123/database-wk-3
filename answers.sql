aa8b231e76fa3f82908bd504f35ed67cd6296025
Mariga <johnmariga8@gmail.com>
2025-09-15 17:32:39 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,1 +1,15 @@
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+INSERT INTO student (id, fullName, age)
+VALUES 
+(1, 'Alice Ndungu', 19),
+(2, 'Brian Ochiend', 18),
+(3, 'Catherine Cate', 21);
 
+
+UPDATE student
+SET age = 20
+WHERE id = 2;
